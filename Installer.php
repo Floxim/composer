2432f43b1c56ecd1ae9574a935fdb3661e204a55
Ilya Myasin <dubr.cola@gmail.com>
2015-03-06 13:29:22 +0300

    path resolving improved

--- Installer.php
+++ Installer.php
@@ -9,71 +9,57 @@ class Installer extends LibraryInstaller
 {
    
     protected $types = array(
-        'floxim-module' => 'module/{$vendor}/{$name}/',
-        'floxim-theme' => 'theme/{$vendor}/{$name}/',
-        'floxim-themes' => 'theme/{$name}/'
+        'floxim-module',
+        'floxim-theme',
+        'floxim-themes'
     );
     
-  /**
-   * {@inheritDoc}
-   */
-  public function getInstallPath(PackageInterface $package)
-  {
-      $type = $package->getType();
-      $pattern = $this->types[$type];
-      
-      $vars = array(
-        'type' => $type,
-      );
-      $prettyName = $package->getPrettyName();
-      if (strpos($prettyName, '/') !== FALSE) {
-        $pieces = explode('/', $prettyName);;
-        $vars['vendor'] = $pieces[0];
-        $vars['name'] = $pieces[1];
-      } else {
-        $vars['vendor'] = '';
-        $vars['name'] = $prettyName;
-      }
-      return $this->templatePath($pattern, $vars);
-  }
-  /**
-   * {@inheritDoc}
-   */
-  public function supports($packageType)
-  {
-      return array_key_exists($packageType, $this->types);
-      /**
-      if ($this->composer->getPackage()) {
-        $extra = $this->composer->getPackage()->getExtra();
-        if (!empty($extra['custom-installer'])) {
-          if (!empty($extra['custom-installer'][$packageType])) {
-            return true;
-          }
+    public function nameToPath($type, $name)
+    {
+        $short_type = preg_replace("~^floxim-~", '', $type);
+        if (strpos($name, "/") === false) {
+            $name = "/".$name;
         }
-      }
-      return false;
-      */
-  }
-  /**
-   * Replace vars in a path
-   *
-   * @see Composer\Installers\BaseInstaller::templatePath()
-   *
-   * @param  string $path
-   * @param  array  $vars
-   * @return string
-   */
-  protected function templatePath($path, array $vars = array())
-  {
-      if (strpos($path, '{') !== false) {
-          extract($vars);
-          preg_match_all('@\{\$([A-Za-z0-9_]*)\}@i', $path, $matches);
-          if (!empty($matches[1])) {
-              foreach ($matches[1] as $var) {
-                  $path = str_replace('{$' . $var . '}', $$var, $path);
-              }
-          }
-      }
-      return $path;
-  }
+        
+        list($vendor, $name) = explode("/", $name);
+        $vendor = self::camelize($vendor);
+        $name = self::camelize($name);
+        $name = preg_replace("~^Floxim~", '', $name);
+        $name = preg_replace("~^Module|Themes?~", '', $name);
+        if ($short_type === 'themes') {
+            $res = 'theme/'.$vendor.$name;    
+        } else {
+            $res = $short_type.'/'.$vendor.'/'.$name;
+        }
+        return $res;
+    }
+        
+    public function getInstallPath(PackageInterface $package) 
+    {
+        $type = $package->getType();
+        $name = $package->getPrettyName();
+        return $this->nameToPath($type, $name);   
+    }
+  
+    protected static function camelize($string, $first_upper = true)
+    {
+        $t_string = trim($string, '_');
+        $parts = preg_split('~[_-]~', $t_string);
+        $camelized = '';
+        foreach ($parts as $part_num => $part) {
+            if ($part_num === 0 && $first_upper === false) {
+                $camelized .= $part;
+            } elseif ($part === '') {
+                $camelized .= '_';
+            } else {
+                $camelized .= ucfirst($part);
+            }
+        }
+        return $camelized;
+    }
+  
+    public function supports($packageType)
+    {
+        return in_array($packageType, $this->types);
+    }
 }
